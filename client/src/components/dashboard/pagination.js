5460b0884721abf1d5541b0298e9f8053712b301
Linwen <linwenhuang1313@gmail.com>
2019-02-18 19:43:38 -0500

    added pagination

---
+++ client/src/components/dashboard/pagination.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import Pagination from "material-ui-flat-pagination";
+ 
+const theme = createMuiTheme();
+ 
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { offset: 0 };
+  }
+ 
+  handleClick(offset) {
+    this.setState({ offset });
+  }
+ 
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Pagination
+          limit={10}
+          offset={this.state.offset}
+          total={100}
+          onClick={(e, offset) => this.handleClick(offset)}
+        />
+      </MuiThemeProvider>
+    );
+  }
+}
+ 
+ReactDOM.render(<Example />, document.getElementById("root"));
