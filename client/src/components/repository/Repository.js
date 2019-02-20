26a0d29c47ef45e83610c83c6bc470342072a011
Murad Akhundov <muradakhdev@gmail.com>
2019-02-20 02:58:24 -0500

    added some boilerplate code to repo

--- client/src/components/repository/Repository.js
+++ client/src/components/repository/Repository.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import { withStyles, Button, Grid } from '@material-ui/core';
+import React, {Component} from 'react';
+import {withStyles, Button, Grid, Typography} from '@material-ui/core';
 
 const styles = {
+    repoViewContainer: {
+        paddingTop: 100
+    }
 
 };
 
 class Repository extends Component {
-  render() {
-    // Temporary
-    return <Grid container
-          direction="column"
-          alignItems="center"
-          justify="center"
-    >
-      {this.props.match.params.name}
-    </Grid>
-  }
+
+    getData = () => {
+        const id = this.props.match.params.id;
+        return {
+            name: `Repository #${id}`
+        }
+    };
+
+    render() {
+        // Temporary
+        console.log("repo");
+
+        const data = this.getData();
+
+        const { classes } = this.props;
+
+        return (
+            <Grid item
+                  container
+                  direction="column"
+                  alignItems="center"
+                  justify="center"
+                  spacing={16}
+                  direction="column"
+                  className={classes.repoViewContainer}
+            >
+                <Grid item>
+                    <Typography variant="h3">
+                        {data.name}
+                    </Typography>
+
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default withStyles(styles)(Repository);
