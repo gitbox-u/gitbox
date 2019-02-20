d261bfdc42991cde10e9965680b25676392944a0
Murad Akhundov <muradakhdev@gmail.com>
2019-02-20 01:54:11 -0500

    merged hell

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Button, Card, TextField, withStyles, Grid, Typography, Tab, Tabs, Paper } from '@material-ui/core';
-import { connect } from 'react-redux';
-import { updateLoginField, tryLogin } from '../reducers/loginReducer';
-import { withRouter } from "react-router-dom";
+import React, {Component} from 'react';
+import {Button, Card, TextField, withStyles, Grid, Typography, Tab, Tabs, Paper} from '@material-ui/core';
+import {connect} from 'react-redux';
+import {updateLoginField, tryLogin} from '../reducers/loginReducer';
+import {withRouter} from "react-router-dom";
 import logo from '../assets/logo.svg';
 
 const styles = {
@@ -54,70 +54,16 @@ class Login extends Component {
     handleSubmit = event => {
         this.props.tryLogin();
         this.routeChange("dashboard")();
-    }
+    };
 
     render() {
 
-        const { classes } = this.props;
-        const { username, password } = this.props;
+        const {classes} = this.props;
+        const {username, password} = this.props;
 
         return (
-            <Grid
-                container
-                alignItems="center"
-                spacing={0}
-                justify="center"
-                direction="column"
-                style={{ minHeight: '85vh' }}
-            >
-                <Grid item>
-                    <Paper className={classes.formContainer}>
-                        <Grid container
-                            direction="column"
-                            alignItems="center"
-                            spacing={8}
-                            justify="center">
-                            <Grid item>
-                                <img src={logo} className={classes.logo} />
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="h4" className={classes.logoLabel}>
-                                    GITBOX
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    InputProps={{
-                                        disableUnderline: true,
-                                    }}
-                                    value={username}
-                                    onChange={this.handleChange("username")}
-                                    fullWidth
-                                    placeholder="Username"
-                                    className={classes.formInput}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    InputProps={{
-                                        disableUnderline: true,
-                                    }}
-                                    value={password}
-                                    onChange={this.handleChange("password")}
-                                    fullWidth
-                                    type="password"
-                                    placeholder="Password"
-                                    className={classes.formInput}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <Button fullWidth variant="outlined" color="primary" onClick={this.handleSubmit}
-                                    className={classes.formButton}>Login</Button>
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                </Grid>
             <div>
+                <div id={"bg"}></div>
                 <Grid
                     container
                     alignItems="center"
@@ -167,17 +113,19 @@ class Login extends Component {
                                     />
                                 </Grid>
                                 <Grid item>
-                                    <Button fullWidth variant="outlined" color="primary"
+                                    <Button fullWidth variant="outlined" color="primary" onClick={this.handleSubmit}
                                             className={classes.formButton}>Login</Button>
                                 </Grid>
                             </Grid>
                         </Paper>
                     </Grid>
+                </Grid>
+            </div>
 
-            </Grid>
+
         );
     }
-}
+};
 
 const mapDispatchToProps = {
     updateLoginField,
@@ -185,11 +133,11 @@ const mapDispatchToProps = {
 };
 
 const mapStateToProps = (state) => {
-    const { username, password } = state;
+    const {username, password} = state;
 
-  return {
-    username, password
-  }
+    return {
+        username, password
+    }
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Login)));
