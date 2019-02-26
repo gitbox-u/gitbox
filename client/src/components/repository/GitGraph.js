a55ec2169d7350be960cc114465567af457d0211
Howard Chen <elongateddanishtoaster@gmail.com>
2019-02-25 23:10:09 -0500

    Completed the git graph spec :)

--- client/src/components/repository/GitGraph.js
+++ client/src/components/repository/GitGraph.js
@@ -1,2 +1,4 @@
-// git log --all --date-order --pretty="%h|%p|%d"
+// git log --all --date-order --pretty="%h|%p|%d|%s"
 // empty, but wanted to keep this handy :)
+
+// outputs: "hash | previous commits (parents) | (?) | message"
