7b565ed0c5708c526a3ef47e414a381adae16536
Eric Lindau <eric.lindau1@gmail.com>
2019-03-04 21:02:40 -0500

    Revert geo.js - no longer crashes, bad style

--- client/src/geo/geo.js
+++ client/src/geo/geo.js
@@ -1,21 +1,21 @@
 //source: https://codepen.io/laurentschoonbrodt/pen/NqdPpy
 export const geometric = () => {
-  let refreshDuration = 10000;
-  let refreshTimeout;
-  let numPointsX;
-  let numPointsY;
-  let unitWidth;
-  let unitHeight;
-  let points;
+  var refreshDuration = 10000;
+  var refreshTimeout;
+  var numPointsX;
+  var numPointsY;
+  var unitWidth;
+  var unitHeight;
+  var points;
 
 
   function onLoad() {
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', window.innerWidth);
     svg.setAttribute('height', window.innerHeight);
     document.querySelector('#bg').appendChild(svg);
 
-    let unitSize = (window.innerWidth + window.innerHeight) / 20;
+    var unitSize = (window.innerWidth + window.innerHeight) / 20;
     numPointsX = Math.ceil(window.innerWidth / unitSize) + 1;
     numPointsY = Math.ceil(window.innerHeight / unitSize) + 1;
     unitWidth = Math.ceil(window.innerWidth / (numPointsX - 1));
@@ -23,49 +23,49 @@ export const geometric = () => {
 
     points = [];
 
-    for (let y = 0; y < numPointsY; y++) {
-      for (let x = 0; x < numPointsX; x++) {
+    for (var y = 0; y < numPointsY; y++) {
+      for (var x = 0; x < numPointsX; x++) {
         points.push({x: unitWidth * x, y: unitHeight * y, originX: unitWidth * x, originY: unitHeight * y});
       }
     }
 
     randomize();
 
-    for (let i = 0; i < points.length; i++) {
-      if (points[i].originX !== unitWidth * (numPointsX - 1) && points[i].originY !== unitHeight * (numPointsY - 1)) {
-        let topLeftX = points[i].x;
-        let topLeftY = points[i].y;
-        let topRightX = points[i + 1].x;
-        let topRightY = points[i + 1].y;
-        let bottomLeftX = points[i + numPointsX].x;
-        let bottomLeftY = points[i + numPointsX].y;
-        let bottomRightX = points[i + numPointsX + 1].x;
-        let bottomRightY = points[i + numPointsX + 1].y;
+    for (var i = 0; i < points.length; i++) {
+      if (points[i].originX != unitWidth * (numPointsX - 1) && points[i].originY != unitHeight * (numPointsY - 1)) {
+        var topLeftX = points[i].x;
+        var topLeftY = points[i].y;
+        var topRightX = points[i + 1].x;
+        var topRightY = points[i + 1].y;
+        var bottomLeftX = points[i + numPointsX].x;
+        var bottomLeftY = points[i + numPointsX].y;
+        var bottomRightX = points[i + numPointsX + 1].x;
+        var bottomRightY = points[i + numPointsX + 1].y;
 
-        let rando = Math.floor(Math.random() * 2);
+        var rando = Math.floor(Math.random() * 2);
 
-        for (let n = 0; n < 2; n++) {
-          let polygon = document.createElementNS(svg.namespaceURI, 'polygon');
+        for (var n = 0; n < 2; n++) {
+          var polygon = document.createElementNS(svg.namespaceURI, 'polygon');
 
-          if (rando === 0) {
-            if (n === 0) {
+          if (rando == 0) {
+            if (n == 0) {
               polygon.point1 = i;
               polygon.point2 = i + numPointsX;
               polygon.point3 = i + numPointsX + 1;
               polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + bottomRightX + ',' + bottomRightY);
-            } else if (n === 1) {
+            } else if (n == 1) {
               polygon.point1 = i;
               polygon.point2 = i + 1;
               polygon.point3 = i + numPointsX + 1;
               polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + topRightX + ',' + topRightY + ' ' + bottomRightX + ',' + bottomRightY);
             }
-          } else if (rando === 1) {
-            if (n === 0) {
+          } else if (rando == 1) {
+            if (n == 0) {
               polygon.point1 = i;
               polygon.point2 = i + numPointsX;
               polygon.point3 = i + 1;
               polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + topRightX + ',' + topRightY);
-            } else if (n === 1) {
+            } else if (n == 1) {
               polygon.point1 = i + numPointsX;
               polygon.point2 = i + 1;
               polygon.point3 = i + numPointsX + 1;
@@ -74,7 +74,7 @@ export const geometric = () => {
           }
           polygon.setAttribute('fill', 'rgba(0,0,0,' + (Math.random() / 5) + ')');
           // polygon.setAttribute('stroke','rgba(250,250,250,'+(Math.random()/1)+')');
-          let animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+          var animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
           animate.setAttribute('fill', 'freeze');
           animate.setAttribute('attributeName', 'points');
           animate.setAttribute('dur', refreshDuration + 'ms');
@@ -84,17 +84,17 @@ export const geometric = () => {
         }
       }
     }
-    // let ktmp;
-    // let k;
+    var ktmp;
+    var k;
     refresh();
   }
 
   function randomize() {
-    for (let i = 0; i < points.length; i++) {
-      if (points[i].originX !== 0 && points[i].originX !== unitWidth * (numPointsX - 1)) {
+    for (var i = 0; i < points.length; i++) {
+      if (points[i].originX != 0 && points[i].originX != unitWidth * (numPointsX - 1)) {
         points[i].x = points[i].originX + Math.random() * unitWidth - unitWidth / 2;
       }
-      if (points[i].originY !== 0 && points[i].originY !== unitHeight * (numPointsY - 1)) {
+      if (points[i].originY != 0 && points[i].originY != unitHeight * (numPointsY - 1)) {
         points[i].y = points[i].originY + Math.random() * unitHeight - unitHeight / 2;
       }
     }
@@ -102,31 +102,30 @@ export const geometric = () => {
 
   function refresh() {
     randomize();
-    for (let i = 0; i < document.querySelector('#bg svg').childNodes.length; i++) {
-      let polygon = document.querySelector('#bg svg').childNodes[i];
-      if (polygon && points) {
-        let animate = polygon.childNodes[0];
-        if (animate.getAttribute('to')) {
-          animate.setAttribute('from', animate.getAttribute('to'));
-        }
-        animate.setAttribute('to', points[polygon.point1].x + ',' + points[polygon.point1].y + ' ' + points[polygon.point2].x + ',' + points[polygon.point2].y + ' ' + points[polygon.point3].x + ',' + points[polygon.point3].y);
-        // animate.beginElement();
+    for (var i = 0; i < document.querySelector('#bg svg').childNodes.length; i++) {
+      var polygon = document.querySelector('#bg svg').childNodes[i];
+      var animate = polygon.childNodes[0];
+      if (animate.getAttribute('to')) {
+        animate.setAttribute('from', animate.getAttribute('to'));
       }
+      animate.setAttribute('to', points[polygon.point1].x + ',' + points[polygon.point1].y + ' ' + points[polygon.point2].x + ',' + points[polygon.point2].y + ' ' + points[polygon.point3].x + ',' + points[polygon.point3].y);
+      animate.beginElement();
     }
     refreshTimeout = setTimeout(function () {
       let a = document.getElementById("bg");
-      if (a === null) return;
+      if(a === null) return;
       refresh();
     }, refreshDuration);
   }
 
   function onResize() {
-    let bg = document.querySelector('#bg svg');
-    if (bg) bg.remove();
+    document.querySelector('#bg svg').remove();
     clearTimeout(refreshTimeout);
     onLoad();
   }
 
   onLoad();
   window.onresize = onResize;
-};
+}
+
+
