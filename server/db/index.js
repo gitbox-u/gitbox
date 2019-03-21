85c92a6aefd5fdced1d122f45891df2462a4b946
Eric Lindau <eric.lindau1@gmail.com>
2019-03-20 23:50:52 -0400

    Easy DB interfaces - room for improvement

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,16 @@
+const uuid = require('uuid/v4');
+
 const mongoose = require('mongoose');
 const { Schema, connect, model } = mongoose;
 
 const init = () => connect('mongodb://localhost:27017').then(() => {
   console.log('connected');
+
+  // addEntity();
+  // getEntity('test123').then(console.log);
+  // addRepo('test123', 'TEST', 'URL');
+  // getRepo('test456').then(console.log);
+  // getEntity('test123').then(console.log);
 });
 
 // TODO: Move Schema/model definitions into logical files & use this to export them
@@ -13,23 +21,74 @@ const init = () => connect('mongodb://localhost:27017').then(() => {
 const Repository = model('Repository', new Schema({
   uuid: String, // TODO: See if we can validate the length of this
   name: String,
+  auth: { // TODO: More persistent thing for this (maybe as a file/config?)
+    username: String,
+    password: String,
+    privateKey: String,
+  },
   remoteUrl: String,
   stats: String, // TODO: Expand into useful objects
 }));
 
 /**
- * An entity that may perform operations on repositories
+ * Any entity that may perform operations on repositories
  * (usually a human user).
  */
 const Entity = model('Entity', new Schema({
   uuid: String, // TODO: How do we ensure this matches auth UUIDs?
   authorized: [ // Authorized repo UUIDs
-    {type: String},
+    { type: String },
   ],
 }));
+
+const addEntity = () => {
+  const entityRecord = new Entity({
+    uuid: 'test123',
+    authorized: [],
+  });
+  entityRecord.save(); // TODO: Error check
+};
+
+const getEntity = (uuid) => Entity.findOne({ uuid });
+
+/**
+ * Adds a repository record for an entity.
+ */
+const addRepo = (entityUUID, name, remoteUrl) => {
+  // TODO: Add fingerprint to check if repo already exists - hash remoteUrl / something else?
+  const repoRecord = new Repository({
+    uuid: uuid(),
+    name,
+    auth: {
+      username: '',
+      password: '',
+      privateKey: '',
+    },
+    remoteUrl,
+    stats: '',
+  });
+  repoRecord.save(); // TODO: Error check
+
+  getEntity(entityUUID).then((entity) => {
+    entity.authorized.push(repoRecord.uuid);
+    entity.save();
+  });
+};
+
+/**
+ * Retrieves a repository record by its UUID.
+ *
+ * @returns {Repository} or {null}.
+ */
+// TODO: Restrict info (.findOne().select()...)
+const getRepo = (uuid) => Repository.findOne({ uuid });
 
 module.exports = {
   init,
   Repository,
-  Entity
+  Entity,
+  addEntity,
+  getEntity,
+  addRepo,
+  getRepo
 };
