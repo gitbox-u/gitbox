97170b7e62793833d440f3e0397383ebc1868b88
Eric Lindau <eric.lindau1@gmail.com>
2019-03-22 12:28:38 -0400

    FS recursive mkdir

--- server/git/index.js
+++ server/git/index.js
@@ -3,18 +3,20 @@ const { getRepoRemote } = require('../db/index');
 const { root } = require('../env');
 const path = require('path');
 const fs = require('fs');
+const shell = require('shelljs');
 
 const registerRepo = (remote) => {
   getRepoRemote(remote).then((r) => {
-    if (!r.uuid)  {
+    if (!r.uuid) {
       return;
     } // Assuming repo is added before this (i.e. in DB with 'remote' field)
 
-    const store = path.join(root, r.uuid, 'repo');
+    const store = path.join(root, r.uuid);
     if (fs.existsSync()) {
       git(store).pull('origin', 'master').then(console.log); // TODO: Handle this
     } else { // Does not yet exist
-      git(store).clone(remote, '').then(() => {
+      shell.mkdir('-p', store);
+      git(store).clone(remote, null).then(() => {
         console.log('Cloned ' + remote);
       }).catch((err) => {
         console.log('Cloning error: ' + err);
