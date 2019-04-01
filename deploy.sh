6a5c3319416324806dd30faf0cf186ee73f473f2
Eric Lindau <eric.lindau1@gmail.com>
2019-04-01 18:57:28 -0400

    Automated deployment

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker save -o ~/Desktop/gitmap team01_web
 sudo scp -i "$HOME/Gitmap.pem" ~/Desktop/gitmap ec2-user@ec2-3-87-7-156.compute-1.amazonaws.com:~
 
-ssh -i "$HOME/Gitmap.pem" ~/Desktop/gitmap ec2-user@ec2-3-87-7-156.compute-1.amazonaws.com 'bash -s' < payload.sh
+sudo ssh -i "$HOME/Gitmap.pem" ec2-user@ec2-3-87-7-156.compute-1.amazonaws.com 'bash -s' < deploy_payload.sh
