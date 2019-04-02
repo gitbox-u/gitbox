6fbcd936ba96dbb171f2a9049a9ab280858b123c
Eric Lindau <eric.lindau1@gmail.com>
2019-04-02 11:37:52 -0400

    Remove logs

--- server/env/index.js
+++ server/env/index.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 const root = path.join(process.env.REPODIR ? process.env.REPODIR : require('os').homedir(), 'REPODUMP', 'Repos');
-console.log('rt');
-console.log(root);
 
 module.exports = {
   root,
