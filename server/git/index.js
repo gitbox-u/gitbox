50bc5f1ead0b719a75f54ee4a61dbee9c2538950
Murad Akhundov <muradakhdev@gmail.com>
2019-04-01 20:59:28 -0400

    reworked git stuff

--- server/git/index.js
+++ server/git/index.js
@@ -1,44 +1,53 @@
 const git = require('simple-git/promise');
-const { getRepoRemote } = require('../db/index');
-const { root } = require('../env');
+const {getRepoRemote} = require('../db/index');
+const {root} = require('../env');
 const path = require('path');
 const fs = require('fs');
 const shell = require('shelljs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-const registerRepo = (remote, auth) => {
-  return new Promise((resolve, reject) => {
-    getRepoRemote(remote)
-      .exec()
-      .then((r) => {
-        if (!r.uuid) reject();  // Assuming repo is added before this (i.e. in DB with 'remote' field)
+const registerRepo = (repo) => {
+  if (!repo.uuid) return Promise.reject();  // Assuming repo is added before this (i.e. in DB with 'remote' field)
 
-        const store = path.join(root, r.uuid, 'repo');
-        if (fs.existsSync(store)) { // $ROOT/r.uuid/repo/REPO DATA
-          git(store)
-            .pull('origin', 'master')
-            .then(console.log)
-            .catch(reject);// TODO: Handle this
-          resolve()
-        } else { // Does not yet exist
-          if (auth) remote = `https://${auth.username}:${auth.password}@${remote.split('://')[1]}`;
-          shell.mkdir('-p', store);
-          console.log('dir created');
-          git(store)
-            .clone(remote, null)
-            .then(() => {
-              resolve();
-            })
-            .catch((err) => {
-              reject('Cloning error: ' + err);
-            });
-        }
-      })
-      .catch((err) => {
-        reject();
-        console.log(err)
-      })
-  });
+  const store = path.join(root, repo.uuid, 'repo');
+
+  let remote = repo.remoteUrl;
+  if (repo.auth) remote = `https://${repo.auth.username}:${repo.auth.password}@${remote.split('://')[1]}`;
+  shell.mkdir('-p', store);
+
+  const creds = `cd ${store}/${repo.name}
+        git config credential.helper store`;
+
+  console.log('dir created');
+  return git(store)
+    .clone(remote, repo.name)
+    .then(() => exec(creds))
+    .then(() => Promise.resolve(repo.uuid))
+    .catch((err) => {
+      Promise.reject('Cloning error: ' + err);
+    });
 };
+
+function pullRepo(repo) {
+  const store = path.join(root, repo.uuid, 'repo');
+
+  return git(store)
+    .pull('origin', 'master')
+
+}
+
+async function refreshStats(repoID) {
+
+}
+
+async function getStats(repoID) {
+
+}
+
+async function getStatsCommitter(repoID, committer) {
+
+}
 
 module.exports = {
   registerRepo
