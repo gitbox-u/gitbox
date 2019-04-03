0025d8c71e504e6bed612d21165f5eb32c3a8f54
EDToaster <elongateddanishtoaster@gmail.com>
2019-04-03 19:49:15 -0400

    Added more extensions, stopped XML and JSON from being a thing

--- scripts/Languages.js
+++ scripts/Languages.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 const map = {};
 
 for (let language in languages) {
+  if (language === "XML" || language === "JSON") continue;
   const extensions = languages[language].extensions;
   if (extensions === undefined) continue;
   for(let extension of languages[language].extensions) {
