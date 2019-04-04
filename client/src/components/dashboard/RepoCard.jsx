24ab80f7381143e5bf1bcfe5bb8e78a67f213d42
EDToaster <elongateddanishtoaster@gmail.com>
2019-04-04 00:07:43 -0400

    Rude linwen deleted my commits

--- client/src/components/dashboard/RepoCard.jsx
+++ client/src/components/dashboard/RepoCard.jsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {withStyles} from '@material-ui/styles';
-import {ButtonBase, Card, CardHeader, Grid} from '@material-ui/core';
-import {withRouter} from 'react-router-dom';
-import {getRandomColor} from '../../api/colours';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
+import { ButtonBase, Card, CardHeader, Grid, Badge } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import { deleteRepo } from '../../api/api';
 import Language from './Language';
 import Close from '@material-ui/icons/Close'
 
@@ -22,7 +22,10 @@ const styles = {
   full: {
     width: '100%',
   },
-  
+
+  grow: {
+    flexGrow: 1,
+  },
 };
 
 class RepoCard extends Component {
@@ -41,56 +44,77 @@ class RepoCard extends Component {
   };
 
   toggleRaised = () => {
-    this.setState({raised: !this.state.raised});
+    this.setState({ raised: !this.state.raised });
   };
 
+  deleteRepo = id => {
+    if (window.confirm(`Delete repository ${id}?`)) {
+      deleteRepo(id).then(
+        () => window.location.reload()
+      )
+    }
+  }
+
   render() {
-    const {classes, name, desc, id, breakdown, allLangs} = this.props;
+    const { classes, name, desc, id, breakdown } = this.props;
 
     return (
-      <ButtonBase onClick={this.routeChange(`/repository/${id}`)}
-                  className={classes.full}
-      >
-        <Card
-          className={classes.repoCard}
+      <Badge badgeContent={
+        <ButtonBase onClick={() => this.deleteRepo(id)}>
+            <Close style={{ marginRight: "10px" }}></Close>
+          </ButtonBase>
+          }>
+
+        <ButtonBase onClick={this.routeChange(`/repository/${id}`)}
+            className={classes.full}
+          >
+            <Card
+              className={classes.repoCard}
               onMouseOver={this.toggleRaised}
               onMouseOut={this.toggleRaised}
               raised={this.state.raised}
-        >
-        <Grid container
-            flexDirection= 'row'
-            justifyContent= 'space-between'
-            alignItems= 'center'
-        >
-          <CardHeader
-            className={classes.header}
-            title={name.substring(0,20)}
-            subheader={desc}
-          />
-        </Grid>
+            >
+              <Grid container
+                flexDirection='row'
+                justifyContent='space-between'
+                alignItems='center'
+              >
+                <Grid item
+                  width="1/2"
+                >
+                  <CardHeader
+                    className={classes.header}
+                    title={name.substring(0, 20)}
+                    subheader={desc}
+                  />
+                </Grid>
+                <div className={classes.grow} />
 
-          <Grid container
+              </Grid>
+
+              <Grid container
                 spacing={16}
                 justify='stretch'
                 className={classes.full}
                 alignItems='center'
-          >
-            {
-              breakdown.map(
-                (lang, i) => {
-                  const {language, color} = lang;
+              >
+                {
+                  breakdown.map(
+                    (lang, i) => {
+                      const { language, color } = lang;
 
-                  return (
-                    <Grid item key={i}>
-                      <Language key={2 * i} colour={color} language={language}/>
-                    </Grid>
+                      return (
+                        <Grid item key={i}>
+                          <Language key={2 * i} colour={color} language={language} />
+                        </Grid>
+                      )
+                    }
                   )
                 }
-              )
-            }
-          </Grid>
-        </Card>
-      </ButtonBase>
+              </Grid>
+            </Card>
+          </ButtonBase>
+          </Badge>
     );
   }
 }
